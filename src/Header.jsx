0e3780e74dc8463a20bb3de1e7f8cf901812275b
codingUser96 <abarajithan96@gmail.com>
2020-09-17 13:48:05 +0530

    first commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,29 +1,16 @@
-import React, { Component, } from 'react'
+import React from 'react'
 
-class Header extends Component {
-    constructor (props) {
-        super(props)
-        
-        this.state = {
-            
-        }
-
-    }
-
-    render () {
-    
-        return (
-            <div className="fx-l-header">
-                <div className="fx-navbar fx-navbar--desktop">
-                    <div className="fx-navbar-container">
-                        <nav className="fx-navbar-links">
-                        </nav>
-                    </div>
-                </div>
+const Header = props => {
+    return (
+        <div className="fx-l-header">
+        <div className="fx-navbar fx-navbar--desktop">
+            <div className="fx-navbar-container">
+                <nav className="fx-navbar-links">
+                </nav>
             </div>
-        )
-    }
-
+        </div>
+    </div>
+    )
 }
 
 export default Header;
