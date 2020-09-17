52ebaf34c3b33e28298195e0d83d06469a6f09e8
codingUser96 <abarajithan96@gmail.com>
2020-09-17 23:12:24 +0530

    first commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,45 +2,45 @@ import React from 'react'
 
 const Footer = props => {
     return (
-        <div class="footer page-footer font-small blue pt-4">
-            <div class="container-fluid text-center text-md-left">
-                <div class="row">
-                    <div class="col-md-6 mt-md-0 mt-3">
-                        <h6 class="footer-copyright text-uppercase">Abarajithan</h6>
+        <div className="footer page-footer font-small blue pt-4">
+            <div className="container-fluid text-center text-md-left">
+                <div className="row">
+                    <div className="col-md-6 mt-md-0 mt-3">
+                        <h6 className="footer-copyright text-uppercase">Abarajithan</h6>
                         <h2 className="footer-copyright">Photography</h2>
                     </div>
-                    <hr class="clearfix w-100 d-md-none pb-3"/>
-                    <div class="col-md-3 mb-md-0 mb-3">
-                        {/* <h5 class="footer-copyright text-uppercase">Mail</h5>
-                        <ul class="list-unstyled">
+                    <hr className="clearfix w-100 d-md-none pb-3"/>
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        {/* <h5 className="footer-copyright text-uppercase">Mail</h5>
+                        <ul className="list-unstyled">
                             <li>
                                 <a href="#!">abarajithanpm@gmail.com</a>
                             </li>
                         </ul> */}
                     </div>
-                    <div class="col-md-2 mb-md-0 mb-3">
-                        <h6 class="footer-copyright text-uppercase">Instagram</h6>
-                        <ul class="list-unstyled">
+                    <div className="col-md-2 mb-md-0 mb-3">
+                        <h6 className="footer-copyright text-uppercase">Instagram</h6>
+                        <ul className="list-unstyled">
                         <li>
-                            <a href="https://www.instagram.com/abarajithanpm/?igshid=e193le1nhut8" className="fa fa-instagram">abarajithanpm</a>
+                            <a href="https://www.instagram.com/abarajithanpm/?igshid=e193le1nhut8" className="footerLink fa fa-instagram">abarajithanpm</a>
                         </li>
                         </ul>
-                        <h6 class="footer-copyright text-uppercase">Mail</h6>
-                        <ul class="list-unstyled">
+                        <h6 className="footer-copyright text-uppercase">Mail</h6>
+                        <ul className="list-unstyled">
                             <li>
-                                <a href="#!">abarajithanpm@gmail.com</a>
+                                <a href="#!" className="footerLink">abarajithanpm@gmail.com</a>
                             </li>
                         </ul>
-                        <h6 class="footer-copyright text-uppercase">Contact</h6>
-                        <ul class="list-unstyled">
+                        <h6 className="footer-copyright text-uppercase">Contact</h6>
+                        <ul className="list-unstyled">
                             <li>
-                                <a href="#!">+91 74021 97433</a>
+                                <a href="#!" className="footerLink">+91 74021 97433</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="footer-copyright text-center py-3">© 2020 Copyright: ABARAJITHAN
+            <div className="footer-copyright text-center py-3">© 2020 Copyright: ABARAJITHAN
             </div>
         </div>
     )
